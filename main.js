4efd4656ef154107bd0e1c323bc4584ef8697d94
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-16 10:48:54 +0100

    maj nb lettre

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 //Utilisation de local storage pour stocker les données (mots, essais, etc...)
-const mot = "pende"
+const mot = "pendule"
 const placeLettre = document.querySelector("#word-display")
-const nvlPartie = document.querySelector('.btn-primary')
+// const nvlPartie = document.querySelector('.btn-primary')
 
+window.addEventListener("load", () => {
+
+    if (mot.length > placeLettre.children.length) {
+
+        const nbAjout =  mot.length - placeLettre.children.length
+        console.log(nbAjout,mot.length,placeLettre.children.length)
+
+        for (i = 0; i < nbAjout; i++) {
+            console.log("Boucle ajout")
+            const nvlLettre = document.createElement("span")
+            nvlLettre.classList.add("letter-placeholder")
+            placeLettre.appendChild(nvlLettre)
+        }
+    } else {
+        const nbSuppr = placeLettre.children.length - mot.length
+        console.log(nbSuppr)
+
+        for (i=0; i < nbSuppr ; i++) {
+            const spanSuppr = document.querySelector(".letter-placeholder")
+            spanSuppr.remove();
+        }   
+    } 
+})
 
 document.addEventListener('keydown', (event) => {
     const key = event.key
