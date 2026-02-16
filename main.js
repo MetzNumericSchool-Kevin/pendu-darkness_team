ecc322ddf322ace000ef979d1a2d4e560188867c
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-16 09:37:16 +0100

    premier commit

---
+++ main.js
@@ -0,0 +1,23 @@
+//Utilisation de local storage pour stocker les données (mots, essais, etc...)
+const mot = "pende"
+const placeLettre = document.querySelector("#word-display")
+const nvlPartie = document.querySelector('.btn-primary')
+
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key
+    console.log(key)
+
+    for (i = 0; i < mot.length; i++) {
+        if (mot[i] == key)
+            console.log(`Lettre trouvé à la position ${i}`)
+            placeLettre.children[i + 1].innerHTML = key
+    }
+
+
+
+
+
+
+})
+
