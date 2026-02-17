c9a1f9290edf8c81dc0fb77e6f3c6d916c3f6dbe
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-17 09:25:13 +0100

    ajout badge

--- main.js
+++ main.js
@@ -1,61 +1,65 @@
 //Utilisation de local storage pour stocker les données (mots, essais, etc...)
-const mot = "pendule"
-const placeLettre = document.querySelector("#word-display")
-const nbErreur = document.querySelector("#errors")
-let compteErreur = 0
-const contLettresJouees = document.querySelector("#letters-used")
+const mot = "pendule";
+const placeLettre = document.querySelector("#word-display");
+const nbErreur = document.querySelector("#errors");
+let compteErreur = 0;
+const contLettresJouees = document.querySelector("#letters-used");
 
 // const nvlPartie = document.querySelector('.btn-primary')
 
 window.addEventListener("load", () => {
+	if (mot.length > placeLettre.children.length) {
+		const nbAjout = mot.length - placeLettre.children.length;
+		console.log(nbAjout, mot.length, placeLettre.children.length);
 
-    if (mot.length > placeLettre.children.length) {
+		for (i = 0; i < nbAjout; i++) {
+			console.log("Boucle ajout");
+			const nvlLettre = document.createElement("span");
+			nvlLettre.classList.add("letter-placeholder");
+			placeLettre.appendChild(nvlLettre);
+		}
+	} else {
+		const nbSuppr = placeLettre.children.length - mot.length;
+		console.log(nbSuppr);
 
-        const nbAjout =  mot.length - placeLettre.children.length
-        console.log(nbAjout,mot.length,placeLettre.children.length)
+		for (i = 0; i < nbSuppr; i++) {
+			const spanSuppr = document.querySelector(".letter-placeholder");
+			spanSuppr.remove();
+		}
+	}
+});
 
-        for (i = 0; i < nbAjout; i++) {
-            console.log("Boucle ajout")
-            const nvlLettre = document.createElement("span")
-            nvlLettre.classList.add("letter-placeholder")
-            placeLettre.appendChild(nvlLettre)
-        }
-    } else {
-        const nbSuppr = placeLettre.children.length - mot.length
-        console.log(nbSuppr)
+document.addEventListener("keydown", (event) => {
+	const key = event.key;
+	const lettresJouees = document.createElement("div");
+	lettresJouees.classList.add("badge");
 
-        for (i=0; i < nbSuppr ; i++) {
-            const spanSuppr = document.querySelector(".letter-placeholder")
-            spanSuppr.remove();
-        }   
-    } 
-})
+	console.log(key);
 
-document.addEventListener('keydown', (event) => {
-    const key = event.key
-    console.log(key)
-    
-    if (!/^[a-z]$/.test(key)) {
-        alert("Veuillez taper une lettre entre A et Z !");
-    } else {
-        for (i = 0; i < mot.length; i++) {
-            if (mot[i] == key) {
-                console.log(`Lettre trouvé à la position ${i}`)
-                placeLettre.children[i].textContent = key
-        }   
-    }
-        if (!mot.includes(key)) {
-            console.log (compteErreur)
-            compteErreur += 1
-            nbErreur.textContent = compteErreur + "/5"
-            const pendu = document.querySelector("#error-" + compteErreur)
-            console.log(pendu)
-            pendu.classList.remove("hidden")
-            pendu.classList.add("view")
-        }
-        const lettresJouees = document.createElement("span") 
-        contLettresJouees.appendChild(lettresJouees)
-        lettresJouees.textContent = key
-        }     
-})
-
+	if (!/^[a-z]$/.test(key)) {
+		alert("Veuillez taper une lettre entre A et Z !");
+	} else {
+		for (i = 0; i < mot.length; i++) {
+			if (mot[i] == key) {
+				console.log(`Lettre trouvé à la position ${i}`);
+				placeLettre.children[i].textContent = key;
+				lettresJouees.classList.add("badge-success");
+				contLettresJouees.appendChild(lettresJouees);
+				lettresJouees.innerHTML = key;
+			}
+		}
+		
+		if (!mot.includes(key)) {
+			console.log(compteErreur);
+			compteErreur += 1;
+			nbErreur.textContent = compteErreur + "/5";
+			const pendu = document.querySelector("#error-" + compteErreur);
+			console.log(pendu);
+			pendu.classList.remove("hidden");
+			pendu.classList.add("view");
+			lettresJouees.classList.add("badge-error");
+			contLettresJouees.appendChild(lettresJouees);
+			lettresJouees.innerHTML = key;
+		}
+	}
+});
